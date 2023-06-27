b19be2e24023f7c1efb81433eb86879412b20cf0
Dannirux <dannirux@gmail.com>
2023-06-27 14:57:10 -0500

    cambios a restify

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import express from 'express'
 import cors from "cors"
+import restify from "restify"
 
-const app = express()
-app.use(express.json())
+const app = restify.createServer();
+
+app.use(restify.plugins.bodyParser());
+// const app = express()
+// app.use(express.json())
 
 const PORT = 3008
 
 // app.use(cors)
 
-app.get("/ping", (req, res) => {
+app.get("/ping", (req, res, next) => {
     console.log('damos ping')
     res.setHeader("Content-Type", "application/json")
     res.send("pong")
 })
 
-app.get("/hola/:nombre/:apellido", (req, res) => {
-    console.log('damos ping')
+app.post('/ejemplo', (req:any, res, next) => {
+    console.log('damos ping', req.files, req.body)
+    // Realizar alguna lógica con los datos recibidos
+    // ...
+  
+    res.send({ mensaje: 'Solicitud POST exitosa' });
+  });
+
+app.get("/hola/:nombre/:apellido", (req:any, res, next) => {
+    console.log('damos ping', req.files)
     res.send({ pong: "pong" , nombre: req.params.nombre, apellido: req.params.apellido })
 })
 
